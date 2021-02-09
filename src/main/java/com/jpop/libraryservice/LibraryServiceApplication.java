42cf6e846603f8a4260596c88f58c03a92ab7f39
Ashwani Kumar Singh <ashwanikumarsingh@outlook.com>
2021-02-09 11:19:30 +0530

    zuul gateway

--- src/main/java/com/jpop/libraryservice/LibraryServiceApplication.java
+++ src/main/java/com/jpop/libraryservice/LibraryServiceApplication.java
@@ -3,12 +3,12 @@ package com.jpop.libraryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableCircuitBreaker
 public class LibraryServiceApplication {
 
